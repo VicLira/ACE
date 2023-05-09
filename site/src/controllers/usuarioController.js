f0497865d68698dda4b43871b6f4fee02deb91c8
vicLira <victor.paula@sptech.school>
2023-05-09 20:28:54 -0300

    Estruturada conexão Cadastro e Login com Banco de dados

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,6 +65,8 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var telefone = req.body.telefoneServer;
+    var dtNasc = req.body.dtNascServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,10 +75,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (telefone == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
+    } else if (dtNasc == undefined) {
+        res.status(400).send("Sua data de nascimento está undefined!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, telefone, dtNasc)
             .then(
                 function (resultado) {
                     res.json(resultado);
